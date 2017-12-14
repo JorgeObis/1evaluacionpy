2ae0c0dbe6672189abad38d5e2eed08af7bb7211
jorge juan obis aparicio <33655411+JorgeObis@users.noreply.github.com>
2017-12-14 11:37:17 +0100

    Add files via upload

---
+++ fichero3.cpp
@@ -0,0 +1,17 @@
+//Leer líneas
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main(){
+    ifstream fichero("miFichero.txt");
+    string mensaje;
+    int salir;
+    getline(fichero,mensaje);
+    cout<<"La primera linea dice: ";
+    cout<<mensaje;
+    cin>>salir;
+ 
+    
+    }
